f67b9413dc33d580953768419568e40399d0df31
Jigyasu <133757043+jigyasumxkkxr@users.noreply.github.com>
2024-08-24 00:05:45 +0530

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'assets.aceternity.com',
+          port: '', // leave empty if no specific port is required
+          pathname: '/demos/**', // Adjust pattern to match URL structure
+        },
+        {
+          protocol: 'https',
+          hostname: 'imgtr.ee',
+          port: '', // leave empty if no specific port is required
+          pathname: '/images/**', // Adjust pattern to match URL structure
+        },
+      ],
+    },
+  };
+  
